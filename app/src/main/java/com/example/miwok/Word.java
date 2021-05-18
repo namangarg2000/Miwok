cae4ce221cf121389d73641060f74b9ec4bdb1a4
Naman Garg <namangarg155@gmail.com>
2021-05-18 21:46:52 +0530

    lesson 4 complete

--- app/src/main/java/com/example/miwok/Word.java
+++ app/src/main/java/com/example/miwok/Word.java
@@ -10,10 +10,24 @@ public class Word {
     //Miwok translation
     private String mMiwokTranslation;
 
+    //image resource id
+    private int mImageResourceId = NO_IMAGE_PROVIDED;
+
+    //constant depicting if no image has been passed
+    private static final int NO_IMAGE_PROVIDED = -1;
+    //we use -1 as it is out of the scope of all the resource ids
+
     //Constructor
     public Word(String defaultTranslation, String miwokTranslation){
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+
+    //image resource id is drawable image resource id
+    public Word(String defaultTranslation, String miwokTranslation, int imageResourceId){
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+        mImageResourceId = imageResourceId;
     }
 
     public String getDefaultTranslation() {
@@ -22,5 +36,14 @@ public class Word {
 
     public String getMiwokTranslation() {
         return mMiwokTranslation;
+    }
+
+    public int getImageResourceId(){ return mImageResourceId; }
+
+    /*
+    * Method returns true when image resource has been set
+    * */
+    public boolean hasImage(){
+        return mImageResourceId != NO_IMAGE_PROVIDED;
     }
 }
--- app/src/main/java/com/example/miwok/WordAdapter.java
+++ app/src/main/java/com/example/miwok/WordAdapter.java
@@ -1,25 +1,31 @@
 package com.example.miwok;
 
 import android.app.Activity;
+import android.media.Image;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.core.content.ContextCompat;
 
 import java.util.ArrayList;
 
 public class WordAdapter extends ArrayAdapter<Word> {
 
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+
+    public WordAdapter(Activity context, ArrayList<Word> words, int color) {
         // Here, we initialize the ArrayAdapter's internal storage for the context and the list.
         // the second argument is used when the ArrayAdapter is populating a single TextView.
         // Because this is a custom adapter for two TextViews and an ImageView, the adapter is not
         // going to use this second argument, so it can be any value. Here, we used 0.
         super(context, 0, words);   //calling the super class (ArrayAdapter<Word>) constructor
+        mColorResourceId = color;
     }
 
     /**
@@ -47,6 +53,23 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
         miwokTextView.setText(currentWord.getMiwokTranslation());
+
+        ImageView image = (ImageView) listItemView.findViewById(R.id.image);
+        if(currentWord.hasImage()) {
+            image.setImageResource(currentWord.getImageResourceId());
+        }
+        else{
+            image.setVisibility(View.GONE);
+        }
+
+
+        View textContainer = listItemView.findViewById(R.id.text_container);
+
+        //Find the colour that the resource id maps to
+        int color = ContextCompat.getColor(getContext(),mColorResourceId);
+
+        //Setting the background color of the text view.
+        textContainer.setBackgroundColor(color);
 
         return listItemView;
     }
